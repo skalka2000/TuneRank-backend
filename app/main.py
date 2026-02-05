bc90d8e744ae04984bcf90275f210ba7dae6bced
skalka2000 <piotrpawel.skala@gmail.com>
2026-02-05 03:53:08 +0100

    Allow backend

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://tunerank-frontend.onrender.com"],
+    allow_origins=["http://localhost:3000", "https://tunerank-frontend.onrender.com", "https://tunerank-backend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
